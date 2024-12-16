27451500ab2bfaee12d4573cf5796fc0e5748241
Pedro <m.dossantosataide@ecoles-epsi.net>
2024-12-16 01:21:10 +0100

    new authentification method with home page and start of articles page

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -1,21 +1,45 @@
 <?php
-// src/Controller/RegisterController.php
-
 namespace App\Controller;
+
 use App\Form\UserType;
+use App\Entity\User; // Importez l'entité User
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request; 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
-    #[Route('/register')]
-    public function search(EntityManagerInterface $entityManagerInterface): Response
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $form=$this->createForm(UserType::class);
-        return $this->render('inscription.html.twig',[
-            'form'=>$form->createView(),
+        // Créez un nouvel utilisateur
+        $user = new User();
+
+        // Créez le formulaire lié à l'entité User
+        $form = $this->createForm(UserType::class, $user);
+
+        // Traitez la soumission du formulaire
+        $form->handleRequest($request);
+
+        // Vérifiez si le formulaire a été soumis et est valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            $password = $user->getPassword();
+            $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+            $user->setPassword($hashedPassword);
+
+            // Persistez l'entité en base de données
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            // Redirigez vers une autre page, par exemple la page de connexion après l'enregistrement
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Rendu de la vue avec le formulaire
+        return $this->render('inscription.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,27 +1,54 @@
 <?php
-
 namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
-class User
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $username = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
+    #[ORM\Column(length: 180)]
     private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
+
+    #[ORM\Column(length: 255, unique: true)]
+    #[Assert\NotBlank(message: 'Username cannot be empty.')]
+    #[Assert\Length(
+        min: 3,
+        max: 50,
+        minMessage: 'Your username must be at least {{ limit }} characters long.',
+        maxMessage: 'Your username cannot be longer than {{ limit }} characters.'
+    )]
+    private ?string $username = null;
+
+    /**
+     * @var array<string> The user roles
+     */
+    #[ORM\Column(type: "json")]
+    private array $roles;
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
     private ?string $password = null;
+
+    public function __construct()
+    {
+        // Initialisation explicite de la propriété $roles
+        $this->roles = [];
+    }
 
     public function getId(): ?int
     {
@@ -29,29 +56,62 @@ class User
     }
 
     public function getUsername(): ?string
-    {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): static
-    {
-        $this->username = $username;
-
-        return $this;
-    }
+{
+    return $this->username;
+}
 
     public function getEmail(): ?string
     {
         return $this->email;
     }
 
-    public function setEmail(?string $email): static
+    public function setEmail(string $email): static
     {
         $this->email = $email;
+        return $this;
+    }
+    public function setUsername(string $username): self
+{
+    $this->username = $username;
 
+    return $this;
+}
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     *
+     * @return array<string> List of roles
+     */
+    public function getRoles(): array
+    {
+        // Ajouter un rôle par défaut 'ROLE_USER' si la liste des rôles est vide
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';  // Assurer que chaque utilisateur a au moins ce rôle
+        return array_unique($roles);
+    }
+
+    /**
+     * @param array<string> $roles
+     */
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
         return $this;
     }
 
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
     public function getPassword(): ?string
     {
         return $this->password;
@@ -60,7 +120,15 @@ class User
     public function setPassword(string $password): static
     {
         $this->password = $password;
-
         return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // Si vous avez des données sensibles à effacer, vous pouvez le faire ici.
+        // Exemple : $this->plainPassword = null;
     }
 }
